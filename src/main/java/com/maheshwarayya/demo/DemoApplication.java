2b02c5b6829d46f38dd1c6db44f740eda583ea8a
MaheshR7424 <rmaheshr77@gmail.com>
2020-07-20 15:51:31 +0530

    Added New PrintLine

--- src/main/java/com/maheshwarayya/demo/DemoApplication.java
+++ src/main/java/com/maheshwarayya/demo/DemoApplication.java
@@ -8,6 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to second Projects");
+		System.out.println("This is Spring Boot Maven project");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
