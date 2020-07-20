75de1941abadffc7281dd1e9c7aeaf96656b50d0
MaheshR7424 <rmaheshr77@gmail.com>
2020-07-20 13:45:05 +0530

    Second Project

---
+++ src/main/java/com/maheshwarayya/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.maheshwarayya.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to second Projects");
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
