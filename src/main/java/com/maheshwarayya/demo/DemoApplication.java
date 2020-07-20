0d694eee43daffc1a546d7494a13ba366c9a2f19
MaheshR7424 <rmaheshr77@gmail.com>
2020-07-20 16:04:33 +0530

    One more line added

--- src/main/java/com/maheshwarayya/demo/DemoApplication.java
+++ src/main/java/com/maheshwarayya/demo/DemoApplication.java
@@ -9,6 +9,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		System.out.println("Welcome to second Projects");
 		System.out.println("This is Spring Boot Maven project");
+		System.out.println("This is GitHub Based respository");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
